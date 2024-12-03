6d8ce2d03d2d4154cdb0ec36d6e677be03bdea9a
Jacob <2305180@student.uwtsd.ac.uk>
2024-12-03 12:06:26 +0000

    API initial discussion

---
+++ API intergration.py
@@ -0,0 +1,7 @@
+# API INTERGRATION
+""" -Goals-
+- A combination of APIs
+-shows the current weather, time and location
+-presets include "London", "Paris", "New York", "Moscow", "Los Angeleses" and "Tokyo"
+-include a search function to bring up other locations not in the presets.
+"""
